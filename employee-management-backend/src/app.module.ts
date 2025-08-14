baf7d619a58a60c0a10ce0b588520de41bc3955a
Nicholas Fortune <nfortune03@gmail.com>
2025-08-14 21:20:04 +0700

    init auth module

--- employee-management-backend/src/app.module.ts
+++ employee-management-backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import * as path from 'path';
 import { AdminModule } from './admin/admin.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { PrismaService } from './prisma/prisma.service';
       isGlobal: true,
     }),
     AdminModule,
+    AuthModule,
   ],
   providers: [PrismaService],
 })
