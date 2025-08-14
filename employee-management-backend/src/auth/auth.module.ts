89794d0bc6381bbc3420307f25cd203e2006efa1
Nicholas Fortune <nfortune03@gmail.com>
2025-08-14 21:21:23 +0700

    integrating jwt

--- employee-management-backend/src/auth/auth.module.ts
+++ employee-management-backend/src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtStrategy } from 'src/jwt/jwt.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  providers: [AuthService, PrismaService, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
