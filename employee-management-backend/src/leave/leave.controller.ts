baf6cb4f62ca1cd10a2d812a6dbe33a80561139c
Nicholas Fortune <nfortune03@gmail.com>
2025-08-15 01:18:23 +0700

    implementing leave module

--- employee-management-backend/src/leave/leave.controller.ts
+++ employee-management-backend/src/leave/leave.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Param,
+  Body,
+  Put,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { LeaveService } from './leave.service';
+import { CreateLeaveDto } from './dto/create-leave.dto';
+import { UpdateLeaveDto } from './dto/update-leave.dto';
 
 @Controller('leave')
-export class LeaveController {}
+export class LeaveController {
+  constructor(private readonly leaveService: LeaveService) {}
+
+  @Post()
+  async create(@Body() createLeaveDto: CreateLeaveDto) {
+    const data = await this.leaveService.create(createLeaveDto);
+    return {
+      status: 201,
+      message: 'success create leave',
+      data,
+    };
+  }
+
+  @Get('employee/:employeeId')
+  async getLeavesByEmployee(
+    @Param('employeeId', ParseIntPipe) employeeId: number,
+  ) {
+    const data = await this.leaveService.getLeavesByEmployee(employeeId);
+    return {
+      status: 200,
+      message: 'success get leave data',
+      data,
+    };
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateLeaveDto: UpdateLeaveDto,
+  ) {
+    const data = await this.leaveService.update(id, updateLeaveDto);
+    return {
+      status: 200,
+      message: 'success update leave data',
+      data,
+    };
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    const data = await this.leaveService.delete(id);
+
+    return {
+      status: 200,
+      message: 'success delete leave data',
+      data,
+    };
+  }
+}
