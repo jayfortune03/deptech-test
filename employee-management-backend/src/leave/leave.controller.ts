a1b22099da813308533191f66c1ae1b3f3dac703
Nicholas Fortune <nfortune03@gmail.com>
2025-08-15 01:18:36 +0700

    adding guard for leave module

--- employee-management-backend/src/leave/leave.controller.ts
+++ employee-management-backend/src/leave/leave.controller.ts
@@ -7,12 +7,15 @@ import {
   Put,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { LeaveService } from './leave.service';
 import { CreateLeaveDto } from './dto/create-leave.dto';
 import { UpdateLeaveDto } from './dto/update-leave.dto';
+import { JwtAuthGuard } from 'src/jwt/jwt-auth.guard';
 
 @Controller('leave')
+@UseGuards(JwtAuthGuard)
 export class LeaveController {
   constructor(private readonly leaveService: LeaveService) {}
 
